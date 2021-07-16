9aabf58ab3520443f03bc842be676e213a875ebd
ellislaird <85241858+ellislaird@users.noreply.github.com>
2021-07-16 15:12:01 +0100

    smiley face added?

--- at_location_flutter/lib/common_components/build_marker.dart
+++ at_location_flutter/lib/common_components/build_marker.dart
@@ -2,7 +2,6 @@ import 'package:at_location_flutter/map_content/flutter_map/src/layer/marker_lay
 import 'package:at_location_flutter/location_modal/hybrid_model.dart';
 import 'package:at_location_flutter/utils/constants/colors.dart';
 import 'package:flutter/material.dart';
-
 import 'circle_marker_painter.dart';
 import 'contacts_initial.dart';
 import 'custom_circle_avatar.dart';
@@ -11,65 +10,82 @@ import 'marker_custom_painter.dart';
 Marker buildMarker(HybridModel user,
     {bool singleMarker = false, Widget? marker}) {
   return Marker(
-    anchorPos: AnchorPos.align(AnchorAlign.center),
-    height: 75,
-    width: 50,
-    point: user.latLng!,
-    // ignore: prefer_if_null_operators
-    builder: (ctx) => marker != null
-        ? marker
-        : (singleMarker
-            ? Stack(
-                alignment: Alignment.center,
-                children: [
-                  Positioned(
-                    bottom: 25,
-                    child: SizedBox(
-                      width: 40,
-                      height: 40,
-                      child: CustomPaint(
-                        painter: CircleMarkerPainter(
-                            color: AllColors().WHITE,
-                            paintingStyle: PaintingStyle.fill),
-                      ),
-                    ),
+      anchorPos: AnchorPos.align(AnchorAlign.center),
+      height: 75,
+      width: 50,
+      point: user.latLng!,
+      // ignore: prefer_if_null_operators
+      builder: (ctx) => marker != null
+          ? marker
+          : (singleMarker
+          ? Stack(
+        alignment: Alignment.center,
+        children: [
+          Positioned(
+            bottom: 25,
+            child: SizedBox(
+              width: 40,
+              height: 40,
+              child: CustomPaint(
+                painter: CircleMarkerPainter(
+                    color: AllColors().WHITE,
+                    paintingStyle: PaintingStyle.fill),
+              ),
+            ),
+          ),
+          Positioned(
+            top: 10,
+            child: Icon(
+              Icons.circle,
+              size: 40,
+              color: AllColors().LIGHT_RED,
+            ),
+          ),
+          Positioned(
+            top: 8,
+            child: TextButton(
+              onPressed: () {
+                ///print('My Activity Status: $_status');
+                print('TEST TEST TEST TEST :)');
+              },
+              child: RotationTransition(
+                turns: AlwaysStoppedAnimation(90 / 360),
+                child: Text(
+                  ': )',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 23,
                   ),
-                  Positioned(
-                    top: 10,
-                    child: Icon(
-                      Icons.circle,
-                      size: 40,
-                      color: AllColors().LIGHT_RED,
-                    ),
-                  ),
-                ],
-              )
-            : Stack(
-                alignment: Alignment.center,
-                children: [
-                  Positioned(
-                    bottom: 25,
-                    child: CustomPaint(
-                      size: Size(40, (40 * 1.137455469677715).toDouble()),
-                      painter: RPSCustomPainter(),
-                    ),
-                  ),
-                  Positioned(
-                    top: 10,
-                    child: CircleAvatar(
-                      radius: 15,
-                      backgroundColor: AllColors().LIGHT_RED,
-                      child: user.image != null
-                          ? CustomCircleAvatar(
-                              byteImage: user.image, nonAsset: true, size: 30)
-                          : ContactInitial(
-                              initials: user.displayName,
-                              size: 30,
-                              backgroundColor: AllColors().LIGHT_RED,
-                            ),
-                    ),
-                  ),
-                ],
-              )),
-  );
+                ),
+              ),
+            ),
+          ),
+        ],
+      )
+          : Stack(alignment: Alignment.center, children: [
+        Positioned(
+          bottom: 25,
+          child: CustomPaint(
+            size: Size(40, (40 * 1.137455469677715).toDouble()),
+            painter: RPSCustomPainter(),
+          ),
+        ),
+        Positioned(
+          top: 10,
+          child: CircleAvatar(
+            radius: 15,
+            backgroundColor: AllColors().LIGHT_RED,
+            child: user.image != null
+                ? CustomCircleAvatar(
+                byteImage: user.image, nonAsset: true, size: 30)
+                : ContactInitial(
+              initials: user.displayName,
+              size: 30,
+              backgroundColor: AllColors().LIGHT_RED,
+            ),
+          ),
+        ),
+      ])));
 }
+
